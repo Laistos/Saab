f306091f1b869f51b0a46bb66246ac790cbdf4c1
Akezhan Sotsial <107552938+Laistos@users.noreply.github.com>
2024-05-11 20:25:19 +0500

    added body repair page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MainPage from "./components/MainPage/MainPage";
 import {Route, Routes} from "react-router-dom";
 import Offers from "./components/Offers/Offers";
 import ModelsContainer from "./components/Models/ModelsContainer";
+import BodyRepair from "./components/BodyRepair/BodyRepair";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                     <Route path={'/main'} element={<MainPage />}/>
                     <Route path={'/offers'} element={<Offers />}/>
                     <Route path={'/models'} element={<ModelsContainer />}/>
+                    <Route path={'/repair'} element={<BodyRepair />}/>
                 </Routes>
             </div>
             <Footer/>
